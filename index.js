3f88c9b39b4a397bd3897cb4a123532971985b0a
lKingDGl <alexandrowijaya@yahoo.co.id>
2021-06-23 22:35:10 +0700

    Add surprise

--- index.js
+++ index.js
@@ -10,10 +10,15 @@ const path = require('path');
 
 // For app to use assets folder
 app.use("/public", express.static(path.join(__dirname + "/public")));
+app.use("/assets", express.static(path.join(__dirname + "/assets")));
 
 // GET response
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname + "/views/index.html"));
+});
+
+app.get("/aldebaransamudera", (req, res) => {
+    res.sendFile(path.join(__dirname + "/views/aldebaransamudera.html"));
 });
 
 // Listen to port
