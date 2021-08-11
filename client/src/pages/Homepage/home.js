c41c522cde54ccdd824dadb720066b36263cc807
lKingDGl <alexandrowijaya@yahoo.co.id>
2021-08-11 19:23:54 +0700

    Move index.js inside home.js

--- client/src/pages/Homepage/home.js
+++ client/src/pages/Homepage/home.js
@@ -6,11 +6,59 @@ class Home extends React.Component {
     }
 
     componentDidMount() {
-        const script = document.createElement("script");
-        
-        script.src = "./index.js";
-        script.async = true;
-        document.body.appendChild(script);
+        const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+        const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+        const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+        const header = document.querySelector('.header.container');
+
+        hamburger.addEventListener('click', () => {
+            hamburger.classList.toggle('active');
+            mobile_menu.classList.toggle('active');
+        })
+
+        document.addEventListener('scroll', () => {
+            let scroll_position = window.scrollY;
+            if(scroll_position > 250) {
+                header.style.backgroundColor = "#29323c";
+            } else {
+                header.style.backgroundColor = "transparent";
+            }
+        })
+
+        menu_item.forEach((item) => {
+            item.addEventListener('click', () => {
+                hamburger.classList.toggle('active');
+                mobile_menu.classList.toggle('active');
+            })
+        })
+    }
+
+    componentWillUnmount() {
+        const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+        const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+        const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+        const header = document.querySelector('.header.container');
+
+        hamburger.removeEventListener('click', () => {
+            hamburger.classList.toggle('active');
+            mobile_menu.classList.toggle('active');
+        })
+
+        document.removeEventListener('scroll', () => {
+            let scroll_position = window.scrollY;
+            if(scroll_position > 250) {
+                header.style.backgroundColor = "#29323c";
+            } else {
+                header.style.backgroundColor = "transparent";
+            }
+        })
+
+        menu_item.forEach((item) => {
+            item.removeEventListener('click', () => {
+                hamburger.classList.toggle('active');
+                mobile_menu.classList.toggle('active');
+            })
+        })
     }
 
     render() {
