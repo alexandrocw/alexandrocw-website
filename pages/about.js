7ca54feed36732e4b69e332a880e951f10a4b6e2
Alexandro Christian Wijaya <alexandrowijaya@mail.ugm.ac.id>
2022-01-14 16:13:46 +0700

    Debug apostrophe

--- pages/about.js
+++ pages/about.js
@@ -3,12 +3,12 @@ const About = () => {
     <div>
       <h1>I&apos;m Alex</h1>
       <h2>Full stack web developer</h2>
-      <h3>I'm an Electrical Engineering Undergraduate Student. I have big enthusiast towards technology, web development, and data science. I'm very easy going and can be contacted easily. Honesty and high-self esteem are my principles.</h3>
+      <h3>I&apos;m an Electrical Engineering Undergraduate Student. I have big enthusiast towards technology, web development, and data science. I&apos;m very easy going and can be contacted easily. Honesty and high-self esteem are my principles.</h3>
       <h3>
         Technology I use:
         <ul>
           Front end
-          <li>React js: Robust, easy to use, has a growing community. Lack speed compared than next js, can't do server side rendering</li>
+          <li>React js: Robust, easy to use, has a growing community. Lack speed compared than next js, can&apos;t do server side rendering</li>
           <li>Next js: Good for Search Engine Optimization, built on React, can do server side rendering and static site regeneration</li>
           <li>TailwindCSS: </li>
         </ul>
